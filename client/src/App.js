6a1d3d04eb700b5e24dfd014f9a16a0f47f7a589
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2022-01-07 13:39:43 -0500

    setup router and chakra

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Route, Routes,} from "react-router-dom";
+import {ChakraProvider} from '@chakra-ui/react';
+import Homepage from './pages/Homepage'
 import SideNavBar from "./components/SideNavBar";
 
 function App() {
   return (
-    <>
-      <SideNavBar />
-    </>
+    <ChakraProvider>
+     <Router>
+       <Routes>
+         <Route exact path ="/" element ={<Homepage />}></Route>
+       </Routes>
+     </Router>
+
+    </ChakraProvider>
+     
   );
 }
 
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import SideNavBar from "../components/SideNavBar"
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <SideNavBar />
+    
+      
+    </div>
+  )
+}
+
+export default Homepage

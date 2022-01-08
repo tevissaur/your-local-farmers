afa99eead08f2acd690bcd95140f74ca82f32ce2
unknown <Quentinhnilica@gmail.com>
2022-01-08 11:47:23 -0500

    Sign up mod and header component

---
+++ client/src/components/Signup.js
@@ -0,0 +1,70 @@
+import {useState, useRef} from "react";
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    Input,
+    useDisclosure,
+    Box
+  } from '@chakra-ui/react'
+
+function Signup() {
+const { isOpen, onOpen, onClose } = useDisclosure()
+
+const initialRef = useRef()
+const finalRef = useRef()
+
+return (
+    <>
+
+    <Box>
+        <Button onClick={onOpen} bg="primary.lightGreen" mr="4">
+        Sign Up
+        </Button>
+        <Button bg="primary.lightGreen">Log in</Button>
+    </Box>
+
+    <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+    >
+        <ModalOverlay />
+        <ModalContent>
+        <ModalHeader>Create your account</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+            <FormControl>
+            <FormLabel>First name</FormLabel>
+            <Input ref={initialRef} placeholder='First name' />
+            </FormControl>
+
+            <FormControl mt={4}>
+            <FormLabel>Last name</FormLabel>
+            <Input placeholder='Last name' />
+            </FormControl>
+        </ModalBody>
+
+        <ModalFooter>
+            <Button colorScheme='blue' mr={3}>
+            Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+        </ModalContent>
+    </Modal>
+    </>
+)
+}
+
+export default Signup;
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,6 +1,9 @@
 import SideNavBar from "../components/SideNavBar";
 import { Button, Box, Flex, Heading, Spacer, Center } from "@chakra-ui/react";
 import CategoryCards from "../components/CategoryCards";
+import Header from '../components/header'
+
+
 
 const Homepage = () => {
   return (
@@ -8,18 +11,7 @@ const Homepage = () => {
       <Flex>
         <SideNavBar />
         <Box m={4} flex="1">
-          <Flex >
-            <Center flex="1">
-              <Heading size="md" color="black">Your Local Farmers</Heading>
-            </Center>
-         
-            <Box>
-              <Button bg="primary.lightGreen" mr="4">
-                Sign Up
-              </Button>
-              <Button bg="primary.lightGreen">Log in</Button>
-            </Box>
-          </Flex>
+          <Header></Header>
           <Box>
             <CategoryCards />
           </Box>

50aead729dee7215e273fdafb8134ebfae5e8d13
Alexander Leino <Alexander.Leino@yahoo.com>
2022-01-10 17:09:10 -0500

    fixed links for navbar and quick select items

--- client/src/components/SmallCategoryIcon.js
+++ client/src/components/SmallCategoryIcon.js
@@ -9,15 +9,17 @@ import SmallIcon from '../components/SmallIcon.css'
 
 const SmallCategoryIcon = ({ card }) => {
     return (
-        <Flex justifyContent='space-evenly' flexGrow='3' className="icon-basket">
-            <Box>
-                <Flex flexDir="column" alignItems='center' fontSize='15px' fontWeight='Bold'>
-                    <Box className='smallCategoryIcon' ms={5}>
-                        <Image src={card.image} boxSize='145px' height='125px' width='125px'borderRadius='50%'/>
-                        <Text textAlign="center">{card.title}</Text>
-                    </Box>
-                </Flex>
-            </Box>
+        <Flex justifyContent='space-evenly'className="icon-basket">
+            <Link to={`/products/${card.title.toLowerCase()}`}> 
+                <Box>
+                    <Flex flexDir="column" alignItems='center' fontSize='15px' fontWeight='Bold'>
+                        <Box className='smallCategoryIcon' ms={5}>
+                            <Image src={card.image} boxSize='145px' height='125px' width='125px'borderRadius='50%'/>
+                            <Text textAlign="center">{card.title}</Text>
+                        </Box>
+                    </Flex>
+                </Box>
+            </Link>
         </Flex>
     )
 }
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,5 +1,5 @@
 import SideNavBar from "../components/SideNavBar";
-import { Button, Box, Flex, Heading, Spacer, Center, Text, Container, List, ListItem, OrderedList, UnorderedList, ListIcon } from "@chakra-ui/react";
+import { Button, Box, Flex, Heading, Spacer, Center, Text, Container, List, ListItem, OrderedList, UnorderedList, ListIcon, Link, Image} from "@chakra-ui/react";
 import Header from '../components/Header'
 import SearchBar from '../components/SearchBar'
 import Footer from '../components/Footer'
@@ -7,6 +7,7 @@ import SmartSmallIcon from "../components/SmartSmallIcon";
 import css from './Homepage.css'
 import { BsFillHeartFill } from 'react-icons/bs'
 import customTheme from "../extendedTheme";
+import wheat from '../assets/wheat.jpg'
 
 
 const Homepage = () => {
@@ -33,7 +34,7 @@ const Homepage = () => {
         <Flex justifyContent='Center'>
         </Flex>
         
-        <Flex justifyContent='center' mt={3}>
+        <Flex justifyContent='center' mt={3} flexWrap='wrap'>
           <SmartSmallIcon />
         </Flex>
     </Box>
@@ -68,37 +69,46 @@ const Homepage = () => {
     mt={5}
     background={customTheme.colors.primary.lightGreen}
     borderRadius='25px'
+    width="container.xl"
+    boxShadow="5px 5px 1px 1px #3A7D44"
   >
-  <Flex flexDir='Column'>
+  <Flex flexDir='Column' w='100%' mb={3}>
     <Heading as='h2' fontSize='2rem' textAlign='center' color={customTheme.colors.primary.emeraldGreen}>The Whys Behind The Whats</Heading>
-    <Text>The U.S. food and farming system contributes
-nearly $1 trillion to the national economy—
-or more than 13 percent of the gross domes-
-tic product—and employs 17 percent of the
-labor force.5 With a rapidly increasing world
-population and expanding global markets,
-saving American farmland is a prudent
-investment in world food supply and eco-
-nomic opportunity.
-Asian and Latin American countries are the
-most significant consumers of U.S. agricultur-
-al exports. Latin America, including Mexico,
-purchases an average of about $10.6 billion
-of U.S. agricultural exports each year. Asian
-countries purchase an average of $23.6 bil-
-lion/year, with Japan alone accounting for
-about $10 billion/year.6 Even as worldwide
-demand for a more diverse diet increases,
-many countries are paving their arable land
-to support rapidly expanding economies.
-Important customers today, they are expected
-to purchase more agricultural products in the
-future.
-While domestic food shortages are unlikely in
-the short term, the U.S. Census predicts the
-population will grow by 42 percent in the
-next 50 years. Many developing nations
-already are concerned about food security.</Text>
+        <Text fontWeight='600'>"The U.S. food and farming system contributes
+    nearly $1 trillion to the national economy—
+    or more than 13 percent of the gross domes-
+    tic product—and employs 17 percent of the
+    labor force.5 With a rapidly increasing world
+    population and expanding global markets,
+    saving American farmland is a prudent
+    investment in world food supply and eco-
+    nomic opportunity.
+    Asian and Latin American countries are the
+    most significant consumers of U.S. agricultur-
+    al exports. Latin America, including Mexico,
+    purchases an average of about $10.6 billion
+    of U.S. agricultural exports each year. Asian
+    countries purchase an average of $23.6 bil-
+    lion/year, with Japan alone accounting for
+    about $10 billion/year.6 Even as worldwide
+    demand for a more diverse diet increases,
+    many countries are paving their arable land
+    to support rapidly expanding economies.
+    Important customers today, they are expected
+    to purchase more agricultural products in the
+    future.
+    While domestic food shortages are unlikely in
+    the short term, the U.S. Census predicts the
+    population will grow by 42 percent in the
+    next 50 years. Many developing nations
+    already are concerned about food security."
+    <Link 
+      href='https://sustainable-farming.rutgers.edu/wp-content/uploads/2017/10/Why-Save-Farmland.pdf'
+      target='_blank' fontWeight='bold' color={customTheme.colors.primary.darkGreen}>
+        To read more from this article Click Here!
+    </Link>
+    </Text>
+    <Image borderRadius='25px' padding={1} src={wheat}></Image>
   </Flex>
 </Container>
 

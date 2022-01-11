23d9add2aa7a5b8c5ac5a8b4a4e14dc756a0bd46
Alexander Leino <Alexander.Leino@yahoo.com>
2022-01-10 22:16:13 -0500

    added more stars to farmcards

--- client/src/pages/FarmsPage.js
+++ client/src/pages/FarmsPage.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useQuery } from '@apollo/client'
+import {QUERY_FARM} from '../utils/queries';
 import { Container, Flex, Box } from '@chakra-ui/react'
 import Header from '../components/Header'
 import SideNavBar from '../components/SideNavBar'
 import FarmCard from '../components/FarmCard'
 
 function FarmsPage() {
+    const {loading, data, error} = useQuery(QUERY_FARM)
+    const farmList = data ? data.farms : []
+    console.log(farmList)
+    // console.log(farmList[0].reviews[0].rating)
+    // const reviewAverage = farmList.forEach(farm => {
+    //     console.log(farm.review.rating.length)
+    // })
     return (
     <>
     <Flex>
